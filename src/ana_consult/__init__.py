6dad84e37ffd4fd31d56d3a69b146fce34942201
Daniel Thonon <d.thonon9@gmail.com>
2019-12-27 02:18:52 +0100

    Started analysis script

--- src/ana_consult/__init__.py
+++ src/ana_consult/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
+import gettext
+from pathlib import Path
+from pkg_resources import DistributionNotFound, get_distribution
 
 try:
     # Change here if project is renamed and does not equal the package name
@@ -9,3 +11,9 @@ except DistributionNotFound:
     __version__ = 'unknown'
 finally:
     del get_distribution, DistributionNotFound
+
+# Install gettext for any file in the application
+localedir = Path(__file__).resolve().parent / "locale"
+gettext.bindtextdomain("ana_consult", str(localedir))
+gettext.textdomain("ana_consult")
+_ = gettext.gettext
