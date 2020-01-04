a595f063dd5354f2c9f2239ad096acea89906adf
Daniel Thonon <d.thonon9@gmail.com>
2020-01-05 00:52:01 +0100

    Started clustering

--- src/ana_consult/mtes_analyze.py
+++ src/ana_consult/mtes_analyze.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import pandas as pd
 import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer
 from strictyaml import YAMLValidationError
 
 from ana_consult.ac_conf import AnaConsultConf
@@ -61,6 +62,11 @@ def arguments(args):
     )
     parser.add_argument(
         "--process",
+        help=_("Load pre-processed csv file and do basinc NLP processing"),
+        action="store_true",
+    )
+    parser.add_argument(
+        "--cluster",
         help=_("Load raw csv file from scraper and do first processing"),
         action="store_true",
     )
@@ -83,7 +89,9 @@ def preprocess(config: str):
     """Load raw csv file from scraper and do first processing."""
     logger = logging.getLogger(APP_NAME + ".preprocess")
     pd.set_option("display.max_colwidth", 40)
-    csv_file = Path.home() / ("tmp/" + config.consultation_name + ".csv")
+    csv_file = Path.home() / (
+        "ana_consult/data/raw/" + config.consultation_name + ".csv"
+    )
     logger.info(_("Loading %s"), csv_file)
     data = pd.read_csv(csv_file, header=0, quoting=csv.QUOTE_ALL)
     logger.info(_("Loaded %s rows of raw data"), len(data))
@@ -94,31 +102,104 @@ def preprocess(config: str):
     )
     data = data.drop(columns=["sujet"])
     data = data[["titre", "nom", "date", "heure", "texte"]]
+    csv_file = Path.home() / (
+        "ana_consult/data/interim/" + config.consultation_name + "_split.csv"
+    )
+    data.to_csv(csv_file, index=False, quoting=csv.QUOTE_ALL)
 
     # Drop duplicated lines
-    # print(data[["titre", "nom", "texte"]].head(60))
+    # print(repr(data[["titre", "nom", "texte"]].head(10)))
     data.drop_duplicates(subset=["nom", "texte"], inplace=True)
+    # Drop lines in English (containing wolf)
+    data.drop(data[data.texte.str.contains("wolf", case=False)].index, inplace=True)
     logger.info(_("Storing %s rows of pre-processed data"), len(data))
-    # print(data[["titre", "nom", "texte"]].head(60))
-    csv_file = Path.home() / ("tmp/" + config.consultation_name + "_prep.csv")
+    # print(data[["titre", "nom", "texte"]].head(10))
+    csv_file = Path.home() / (
+        "ana_consult/data/interim/" + config.consultation_name + "_prep.csv"
+    )
     data.to_csv(csv_file, index=False, quoting=csv.QUOTE_ALL)
+
+
+def nlp_convert(nlp, txt):
+    """Convert a text to spacy doc."""
+    try:
+        return nlp(txt)
+    except Exception:
+        print(txt)
+        return nlp("")
 
 
 def process(config: str):
     """Load pre-processed csv file do first base NLP processing."""
     logger = logging.getLogger(APP_NAME + ".process")
     pd.set_option("display.max_colwidth", 120)
-    csv_file = Path.home() / ("tmp/" + config.consultation_name + "_prep.csv")
+    csv_file = Path.home() / (
+        "ana_consult/data/interim/" + config.consultation_name + "_prep.csv"
+    )
     logger.info(_("Loading %s"), csv_file)
-    data = pd.read_csv(csv_file, header=0, quoting=csv.QUOTE_ALL, nrows=5)
-    data["nlp_col"] = data["titre"] + ". " + data["texte"]
-    print(data["nlp_col"].head(60))
+    data = pd.read_csv(csv_file, header=0, quoting=csv.QUOTE_ALL, nrows=100000)
+    data["nlp_col"] = data["titre"].str.lower() + ". " + data["texte"].str.lower()
+
+    # Prepare NLP processing
+    logger.info(_("NLP text processing"))
     nlp = spacy.load("fr_core_news_sm")
-    data["doc"] = data["nlp_col"].apply(lambda x: nlp(x))
-    for doc in data["doc"]:
-        print(doc)
-        for token in doc:
-            print(token.lemma_, token.is_stop, token.is_punct)
+    # Adjust stopwords for this specific topic
+    nlp.Defaults.stop_words |= {
+        "y",
+        "france",
+    }
+    nlp.Defaults.stop_words -= {"contre", "pour"}
+    data["doc"] = data["nlp_col"].apply(lambda t: nlp_convert(nlp, t))
+    # Remove stopwords, ponctuation and spaces
+    data["doc_filt"] = data["doc"].apply(
+        lambda d: [l.lemma_ for l in d if not (l.is_stop or l.is_punct or l.is_space)]
+    )
+
+    # Make a list of processed text
+    doc = data.doc_filt.apply(lambda d: " ".join(d))
+    line = 0
+    for row in data.itertuples():
+        print("--------------------------")
+        print(row.titre + ". " + row.texte)
+        print(doc[line])
+        if line < 10:
+            line += 1
+        else:
+            break
+    # Save data
+    pkl_file = Path.home() / (
+        "ana_consult/data/interim/" + config.consultation_name + "_doc.pkl"
+    )
+    logger.info(_("Storing NLP document to %s"), pkl_file)
+    doc.to_pickle(pkl_file)
+    pkl_file = Path.home() / (
+        "ana_consult/data/interim/" + config.consultation_name + "_nlp.pkl"
+    )
+    logger.info(_("Storing full data to %s"), pkl_file)
+    data.to_pickle(pkl_file)
+
+
+def cluster(config: str):
+    """Perform clustering on NLP processed data."""
+    logger = logging.getLogger(APP_NAME + ".cluster")
+    # Load data
+    pkl_file = Path.home() / (
+        "ana_consult/data/interim/" + config.consultation_name + "_doc.pkl"
+    )
+    logger.info(_("Loading NLP document from %s"), pkl_file)
+    doc = pd.read_pickle(pkl_file)
+    # define vectorizer parameters
+    tfidf_vectorizer = TfidfVectorizer(
+        max_df=0.8, min_df=0.2, stop_words=None, use_idf=True, ngram_range=(1, 3),
+    )
+    # Fit vectoriser to NLP processed column
+    logger.info(_("Fitting TF-IDF vectorizer to NLP data"))
+    tfidf_matrix = tfidf_vectorizer.fit_transform(doc)
+    terms = tfidf_vectorizer.get_feature_names()
+    logger.info(_("TF-IDF (n_samples, n_features): %s"), tfidf_matrix.shape)
+    corpus_index = [n for n in doc]
+    df = pd.DataFrame(tfidf_matrix.todense(), index=corpus_index, columns=terms)
+    print(df[0:10])
 
 
 def main(args):
@@ -194,6 +275,13 @@ def main(args):
         process(ac_ctrl)
         return None
 
+    # Clustering
+    if args.cluster:
+        logger.info(_("Clustering processed file"))
+        cluster(ac_ctrl)
+        return None
+
+    logger.info(_("End of processing"))
     return None
 
 
